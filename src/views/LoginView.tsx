e5560bac3dbff6a08f2fe1b1fcbeafd5797ab03b
puly garcia <pulygarcia09@gmail.com>
2024-12-27 17:43:39 -0300

    starting... creating router

---
+++ src/views/LoginView.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  
+    return (
+      <>
+        <p>Login view</p>
+      </>
+    );
+  }
---
+++ src/views/RegisterView.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  
+    return (
+      <>
+        <p>Register view</p>
+      </>
+    );
+  }
